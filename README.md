4773047a1269f31e37e2a55139e83e7d5d3f97bb
Oblivion233 <lucaaas2333@outlook.com>
2021-03-29 03:04:13 +1300

    implemented signing transactions

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
-js-blockchain
-======
+# js-blockchain
 
 Created a JavaScript blockchain with crypto-js to better understand how blockchain works
 
-Dependencies
-------
+## Dependencies
+
 ### `npm install crypto-js`
 
 To use _SHA-256_ hashing algorithm.
+
+### `npm install elliptic`
+
+To use elliptic library to generate public and private key, sign, and verify signature.
--- src/main.js
+++ src/main.js
@@ -1,134 +1,27 @@
-const SHA256 = require("crypto-js/sha256")
+const { Blockchain, Transaction } = require("./blockchain")
+const EC = require("elliptic").ec
+const ec = new EC("secp256k1")
 
-class Transaction {
-	constructor(fromAddress, toAddress, amount) {
-		this.fromAddress = fromAddress
-		this.toAddress = toAddress
-		this.amount = amount
-	}
-}
-class Block {
-	constructor(timestamp, transactions, previousHash = "") {
-		this.timestamp = timestamp
-		this.transactions = transactions
-		this.previousHash = previousHash
-		this.hash = this.calculateHash()
-		this.nonce = 0
-	}
-
-	calculateHash() {
-		return SHA256(
-			this.previousHash +
-				this.timestamp +
-				JSON.stringify(this.transactions) +
-				this.nonce
-		).toString()
-	}
-
-	mineBlock(difficult) {
-		while (
-			this.hash.substring(0, difficult) !== Array(difficult + 1).join("0")
-		) {
-			this.nonce++
-			this.hash = this.calculateHash()
-		}
-		console.log("Block mined: " + this.hash)
-	}
-}
-
-class Blockchain {
-	constructor() {
-		this.chain = [this.createGenesisBlock()]
-		this.difficult = 3
-		this.pendingTransactions = []
-		this.miningReward = 100
-	}
-
-	createGenesisBlock() {
-		return new Block("01/01/2021", "Genesis block", "0")
-	}
-
-	getLatestBlock() {
-		return this.chain[this.chain.length - 1]
-	}
-
-	minePendingTransactions(miningRewardAddress) {
-		let block = new Block(Date.now(), this.pendingTransactions)
-		block.mineBlock(this.difficult)
-		console.log("Block successfully mined!")
-
-		this.chain.push(block)
-		this.pendingTransactions = [
-			new Transaction(null, miningRewardAddress, this.miningReward),
-		]
-	}
-
-	createTransaction(transaction) {
-		this.pendingTransactions.push(transaction)
-	}
-
-	getBalanceOfAddress(address) {
-		let balance = 0
-		for (const block of this.chain) {
-			for (const trans of block.transactions) {
-				if (trans.fromAddress === address) {
-					balance -= trans.amount
-				}
-				if (trans.toAddress === address) {
-					balance += trans.amount
-				}
-			}
-		}
-		return balance
-	}
-
-	isChainValid() {
-		for (let i = 1; i < this.chain.length; i++) {
-			const currentBlock = this.chain[i]
-			const previousBlock = this.chain[i - 1]
-
-			if (currentBlock.hash !== currentBlock.calculateHash()) {
-				return false
-			}
-			if (currentBlock.previousHash !== previousBlock.hash) {
-				return false
-			}
-		}
-		return true
-	}
-}
+const myKey = ec.keyFromPrivate(
+	"0b65ada561dacb10a528b15c27ac0bdf5b4c4bf4540967c99eff08262dcfb279"
+)
+const myWalletAddress = myKey.getPublic("hex")
 
 let monkeCoin = new Blockchain()
-// console.log("Mining block 1...")
-// monkeCoin.addBlock(new Block(1, "26/03/2021", { amount: 3 }))
-// console.log("Mining block 2...")
-// monkeCoin.addBlock(new Block(2, "27/03/2021", { amount: 6 }))
-// console.log("Mining block 3...")
-// monkeCoin.addBlock(new Block(2, "28/03/2021", { amount: 9 }))
 
-monkeCoin.createTransaction(new Transaction("address1", "address2", 100))
-monkeCoin.createTransaction(new Transaction("address2", "address1", 50))
+const tx1 = new Transaction(myWalletAddress, "public key goes here", 10)
+
+tx1.signTransaction(myKey)
+monkeCoin.addTransaction(tx1)
 
 console.log("Starting the miner...")
-monkeCoin.minePendingTransactions("lucas-address")
+monkeCoin.minePendingTransactions(myWalletAddress)
 
 console.log(
 	"Balance of Lucas is ",
-	monkeCoin.getBalanceOfAddress("lucas-address")
+	monkeCoin.getBalanceOfAddress(myWalletAddress)
 )
 
-console.log("Starting the miner again...")
-monkeCoin.minePendingTransactions("lucas-address")
+monkeCoin.chain[1].transactions[0].amount = 1
 
-console.log(
-	"Balance of Lucas is ",
-	monkeCoin.getBalanceOfAddress("lucas-address")
-)
-
-console.log("Starting the miner again...")
-monkeCoin.minePendingTransactions("lucas-address")
-
-console.log(
-	"Balance of Lucas is ",
-	monkeCoin.getBalanceOfAddress("lucas-address")
-)
+console.log("Is chain valid? ", monkeCoin.isChainValid())
